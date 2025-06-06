b7e6369b6740c3ad9973d374d75cfaa3a356559b
VALENTIN MOINOT <134944251+ValCustomizely@users.noreply.github.com>
2025-06-06 13:30:51 +0200

    Update test_gold_sniper.py

--- test/test_gold_sniper.py
+++ test/test_gold_sniper.py
@@ -1,5 +1,8 @@
 import pytest
 from unittest.mock import AsyncMock, MagicMock
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import main
 from datetime import datetime
 
